28c137884f2139709459d20bbc6c102ec1e2a6bb
Gabrielnolasc <gabrielnolasco831@gmail.com>
2025-08-06 17:40:37 -0300

    solução concluída do encontrandoOSegundoMaiorNumero

--- encontrandoOSegundoMaiorNumero.py
+++ encontrandoOSegundoMaiorNumero.py
@@ -1,11 +1,11 @@
 numeros = [987654321,2,7654321,56,1234567, 1, 88888,3,42,999999,5,1000000000,13,101010,7,444, 9, 2, 13, 9]
 
-def segundoMaiorNumero(lista){
+def segundoMaiorNumero(lista):
   numerosUnicos = liste(set(lista));
   numerosUnicos.sort(reverse=true);
   if len(numerosUnicos) < 2:
     return none;
   return numerosUnicos[1];
-}
+
 
 segundoMaiorNumero(numeros);
