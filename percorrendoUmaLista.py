9d7c20a290049a844d6ecb072049b11e19a3996b
Gabrielnolasc <gabrielnolasco831@gmail.com>
2025-08-06 17:48:32 -0300

    solução concluída do percorrendoUmaLista

--- percorrendoUmaLista.py
+++ percorrendoUmaLista.py
@@ -1,8 +1,7 @@
-numeros = [987654321,2,7654321,56,1234567, 1, 88888,3,42,999999,5,1000000000,13,101010,7,444, 9, 2, 13, 9];
+numeros = [987654321, 2, 7654321, 56, 1234567, 1, 88888, 3, 42, 999999, 5, 1000000000, 13, 101010, 7, 444, 9, 2, 13, 9]
 
-def imrprimirLista(lista){
-   for elemento in  lista:
-      print(elemento)
-}
+def imprimirLista(lista):
+    for elemento in lista:
+        print(elemento)
 
-imprimirLista(numeros);
+print(imprimirLista(numeros))
