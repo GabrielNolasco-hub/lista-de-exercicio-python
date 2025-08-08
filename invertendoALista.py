4703670131b4b640db2181b60b969f69828b123d
Gabrielnolasc <gabrielnolasco831@gmail.com>
2025-08-07 21:08:52 -0300

    solução concluída do invertendoALista

---
+++ invertendoALista.py
@@ -0,0 +1,6 @@
+numeros = [987654321,2,7654321,56,1234567, 1, 88888,3,42,999999,5,1000000000,13,101010,7,444, 9, 2, 13, 9]
+
+def lista_invertida(lista):
+    return lista[::-1]
+
+print(lista_invertida(numeros))
